994e4bf1e72ec87dfbf365aa58b7e89389f6352a
sorocode <tudgus010707@naver.com>
2025-02-03 23:24:55 +0900

    :sparkles: HelloWorldBean 추가

--- src/main/java/com/sorocode/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/sorocode/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -13,4 +13,8 @@ public class HelloWorldController {
     public String helloWorld(){
         return "Hello World";
     }
+    @GetMapping(value = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    }
 }
