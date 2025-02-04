4719783ddb5a0fff89995bb5cb72aa858ed1f1ad
sorocode <tudgus010707@naver.com>
2025-02-04 21:25:17 +0900

    :globe_with_meridians: Internalization 관련 코드

--- src/main/java/com/sorocode/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/sorocode/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,19 +1,38 @@
 package com.sorocode.rest.webservices.restful_web_services.helloworld;
 
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 // Rest API
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     // /hello-world
     // "Hello World"
     @GetMapping(value = "/hello-world")
     public String helloWorld(){
         return "Hello World";
     }
+
+    @GetMapping(value = "/hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        Locale locale= LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null,"Default Message", locale);
+
+    }
+
     @GetMapping(value = "/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello World");
