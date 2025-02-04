3722e036460acc2021c3b68d1f9b48b98bb8f5b2
sorocode <tudgus010707@naver.com>
2025-02-04 14:40:52 +0900

    :sparkles: Path Parameter 추가

--- src/main/java/com/sorocode/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/sorocode/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.sorocode.rest.webservices.restful_web_services.helloworld;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 // Rest API
@@ -16,5 +17,13 @@ public class HelloWorldController {
     @GetMapping(value = "/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello World");
+    }
+
+    // Path parameters
+    // /users/{id}/todos/{id} => /users/2/todos/200
+    // /hello-world/path-variable/{name}
+    @GetMapping(value = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s", name ));
     }
 }
